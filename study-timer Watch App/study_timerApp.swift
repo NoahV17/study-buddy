f8f757b9fe1a401f01baa847a225e06d71c3bcf8
NoahV17 <nvario1@gmail.com>
2023-09-20 12:22:41 -0400

    Initial Commit

---
+++ study-timer Watch App/study_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_timerApp.swift
+//  study-timer Watch App
+//
+//  Created by Noah Vario on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct study_timer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

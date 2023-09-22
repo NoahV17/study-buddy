a1de93c27d675fa9b656a601b052b810d87e2985
NoahV17 <nvario1@gmail.com>
2023-09-22 17:01:42 -0400

    sync github with local repo

--- study-timer Watch App/ContentView.swift
+++ study-timer Watch App/ContentView.swift
@@ -10,17 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Text("Welcome to Study Pal!")
+            Image(systemName: "stopwatch")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .foregroundColor(.green)
+            
+            let timer = Timer()
+            Text("Study time left")
+            Text("25:00")
+
+            Button(action: timer.start) {
+                Text("Start")
+                    .foregroundColor(Color.green)
+            }
+            Button(action: timer.pause) {
+                Text("Pause")
+                    .foregroundColor(Color.yellow)
+            }
         }
         .padding()
+        VStack{
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
+
+
+
--- study-timer Watch App/study_timerApp.swift
+++ study-timer Watch App/study_timerApp.swift
@@ -14,4 +14,47 @@ struct study_timer_Watch_AppApp: App {
             ContentView()
         }
     }
+    
 }
+
+class Timer{
+    //Initialize the timer class with the variable
+    //paused = true, meaning the timer isn't running on start
+    var count = 0
+    var paused = true
+    
+    //begin sets the countdown to 1500(seconds, aka 25 minutes) when user presses the start button
+    func begin(){
+        count = 1500
+    }
+    
+    //start function allows the increment command to start counting down on the second
+    //this is triggered by pressing the start button
+    func start() {
+        paused = false
+    }
+    //pause function puts the timer to a stop when the user presses the pause button
+    func pause() {
+        paused = true
+    }
+    
+    func increment() {
+        if(paused == false){
+            count-=1
+        }
+    }
+    
+    func reset() {
+        count = 0
+    }
+    func time(string: String){
+        //let temp : String = count.stringValue
+        //return temp
+        //converts the count to a string to be returnable in the temp variable
+    }
+}
+
+//@IBAction func buttonAction(){
+    
+
+//func setTitle(_ title: String?)

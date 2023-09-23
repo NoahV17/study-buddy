627ee863116f3abd8ffeffe069edb062ee73bc34
NoahV17 <nvario1@gmail.com>
2023-09-23 09:03:36 -0400

    Added functional counter, buttons correctly work

--- study-timer Watch App/study_timerApp.swift
+++ study-timer Watch App/study_timerApp.swift
@@ -13,47 +13,67 @@ struct study_timer_Watch_AppApp: App {
         WindowGroup {
             ContentView()
         }
+        
     }
+    
     
 }
 
-class Timer{
+
+
+class TimeTrack{
     //Initialize the timer class with the variable
     //paused = true, meaning the timer isn't running on start
-    var count = 0
+    var studyLength = 1430
+    var breakLength = 300
     var paused = true
-    
-    //begin sets the countdown to 1500(seconds, aka 25 minutes) when user presses the start button
-    func begin(){
-        count = 1500
-    }
     
     //start function allows the increment command to start counting down on the second
     //this is triggered by pressing the start button
     func start() {
         paused = false
+        print("start")
     }
     //pause function puts the timer to a stop when the user presses the pause button
     func pause() {
         paused = true
-    }
-    
-    func increment() {
-        if(paused == false){
-            count-=1
-        }
-    }
-    func seconds(int){
-        return count%60
+        print("pause")
     }
     
     func reset() {
-        count = 0
+        studyLength = 0
+        print("reset")
     }
+    
+    func decrement() {
+        if(paused == false){
+            studyLength-=1
+            print(studyLength)
+        }
+    }
+    
+    func seconds() -> String {
+        print(String(studyLength%60))
+        return String(studyLength%60)
+    }
+    func minutes() -> String{
+        print(String(studyLength/60))
+        return String(studyLength/60)
+    }
+    
+    func changeStudyLength(len: Int){
+        studyLength = len
+    }
+    func changeBreakLength(len: Int){
+        breakLength = len
+    }
+    
     func time(string: String){
         //let temp : String = count.stringValue
         //return temp
         //converts the count to a string to be returnable in the temp variable
     }
+    
+   
 }
 

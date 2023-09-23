5c9dcb484af1b558943b2ff5cb9cc89c3698d503
NoahV17 <nvario1@gmail.com>
2023-09-23 13:49:16 -0400

    Better UI using VStack & HStack

--- study-timer Watch App/ContentView.swift
+++ study-timer Watch App/ContentView.swift
@@ -12,42 +12,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    let timer = TimeTrack()
-    //let minutes = 0
-    //let seconds = 0
-        let timeText = timer.minutes()+":"+timer.seconds()
-        
-        
+        let timer = TimeTrack()
         VStack {
-            Text("Welcome to Study Pal!")
-            Image(systemName: "stopwatch")
-                .imageScale(.large)
-                .foregroundColor(.green)
-            
-            
-            Text("Study time left")
-            if(1==1){
-                Text(timeText)
+            VStack{
+                Image(systemName: "stopwatch")
+                    .imageScale(.large)
+                    .foregroundColor(.green)
+                
+                Text("Study time left")
+                ProgressView(value: 10, total: 15).tint(.green)
             }
-            Button("Starts") {
-                timer.start()
-            }.foregroundColor(Color.green)
-            
-            Button(action: timer.pause) {
-                Text("Pause")
-                    .foregroundColor(Color.yellow)
-            }
-            
-            Button(action: timer.decrement) {
-                Text("-1")
-                    .foregroundColor(Color.red)
+            Spacer()
+                .frame(width: 0.0, height: 100.0)
+            HStack{
+                Button(action: timer.start) {
+                    Image(systemName: "play")
+                        .foregroundColor(Color.green)
+                }
+                
+                Button(action: timer.pause) {
+                    Image(systemName: "pause")
+                        .foregroundColor(Color.yellow)
+                }
+                
+                Button(action: timer.decrement) {
+                    Image(systemName: "stop").foregroundColor(Color.red)
+                }
             }
         }
         .padding()
-        VStack{
-            
-        }
-        
+
     }
     
 }
@@ -55,7 +49,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        
     }
 }
 
